6ab7e45aee4f770b2f45418002c3a0f8c6e322f9
Rogan Johnston <100793371+Rowgo@users.noreply.github.com>
2025-07-16 19:51:54 -0700

    encapsulated main functionality inside if
    This means that when the file is imported it won't immediately execute the code, but I don't understand what scenario this is trying to prevent since I don't see any reason why someone would import main anyways.
    
    Need to do more research on this subject.

--- main.py
+++ main.py
@@ -5,32 +5,33 @@ from xml_builder import MTGDatabaseBuilder
 from mtg_sheet import GoogleSheetUrl
 from mtg_set import SetData
 
-DOC_URL = 'https://docs.google.com/spreadsheets/d/1f3jWL21p7YiIwsrQ3gG59_kV5k5blZxJzk8c2dBEe0s'
-EXPORT_TYPE = 'csv'
+if __name__ == '__main__':
+    DOC_URL = 'https://docs.google.com/spreadsheets/d/1f3jWL21p7YiIwsrQ3gG59_kV5k5blZxJzk8c2dBEe0s'
+    EXPORT_TYPE = 'csv'
 
-SETINFO_URL = GoogleSheetUrl(sheet_name='setinfo', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='1329260583')
+    SETINFO_URL = GoogleSheetUrl(sheet_name='setinfo', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='1329260583')
 
-CARDSHEET_URL_LIST = [
-    GoogleSheetUrl(sheet_name='white_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='939138215'),
-    GoogleSheetUrl(sheet_name='blue_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='2066320701'),
-    GoogleSheetUrl(sheet_name='black_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='1190842356'),
-    GoogleSheetUrl(sheet_name='red_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='1155229247'),
-    GoogleSheetUrl(sheet_name='green_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='54239636'),
-    GoogleSheetUrl(sheet_name='multicolor_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='122032064'),
-    GoogleSheetUrl(sheet_name='colorless_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='2064516960')
-]
+    CARDSHEET_URL_LIST = [
+        GoogleSheetUrl(sheet_name='white_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='939138215'),
+        GoogleSheetUrl(sheet_name='blue_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='2066320701'),
+        GoogleSheetUrl(sheet_name='black_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='1190842356'),
+        GoogleSheetUrl(sheet_name='red_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='1155229247'),
+        GoogleSheetUrl(sheet_name='green_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='54239636'),
+        GoogleSheetUrl(sheet_name='multicolor_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='122032064'),
+        GoogleSheetUrl(sheet_name='colorless_cards', doc_url=DOC_URL, export_type=EXPORT_TYPE, sheet_id='2064516960')
+    ]
 
-folder = 'wfa_sheets'
-setinfo_path = SetDownloader.pull_and_save(SETINFO_URL, folder)
+    folder = 'wfa_sheets'
+    setinfo_path = SetDownloader.pull_and_save(SETINFO_URL, folder)
 
-cardsheet_path_list = []
-for cardsheet_url in CARDSHEET_URL_LIST:
-    cardsheet_path = SetDownloader.pull_and_save(cardsheet_url, folder)
-    cardsheet_path_list.append(cardsheet_path)
+    cardsheet_path_list = []
+    for cardsheet_url in CARDSHEET_URL_LIST:
+        cardsheet_path = SetDownloader.pull_and_save(cardsheet_url, folder)
+        cardsheet_path_list.append(cardsheet_path)
 
-set_data: SetData = MTGSheetProcessor.get_setdata(setinfo_path, cardsheet_path_list)
+    set_data: SetData = MTGSheetProcessor.get_setdata(setinfo_path, cardsheet_path_list)
 
-MTGDatabaseBuilder.build_database(set_data)
+    MTGDatabaseBuilder.build_database(set_data)
 
 
 
