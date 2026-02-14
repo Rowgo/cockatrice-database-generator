fce98cab02332e35689f928800a53fc807a89217
Rogan Johnston <100793371+Rowgo@users.noreply.github.com>
2026-02-14 13:50:01 -0800

    moved everything into its own classes

--- src/gui.py
+++ src/gui.py
@@ -7,49 +7,74 @@ root = tk.Tk()
 root.title("Cockatrice Card Set Generator")
 root.geometry("400x300")
 
-sheet_entry_frame = tk.Frame(root)
-sheet_entry_frame.pack(pady=20, padx=20, fill="x")
+class SheetPanel(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.create_widgets()
 
-sheet_label = tk.Label(sheet_entry_frame, text="Google sheet url that holds your card data.", font=("Arial", 12))
-sheet_label.pack(pady=20, anchor="w")
+    def create_widgets(self):
+        sheet_entry_frame = tk.Frame(root)
+        sheet_entry_frame.pack(pady=20, padx=20, fill="x")
 
-sheet_entry = tk.Entry(sheet_entry_frame, width=30)
-sheet_entry.pack(fill="x", expand=True)
+        sheet_label = tk.Label(sheet_entry_frame, text="Google sheet url that holds your card data.", font=("Arial", 12))
+        sheet_label.pack(pady=20, anchor="w")
 
-save_path_frame = tk.Frame(root)
-save_path_frame.pack(pady=20, padx=20, fill="x")
+        sheet_entry = tk.Entry(sheet_entry_frame, width=30)
+        sheet_entry.pack(fill="x", expand=True)
 
-save_path_label = tk.Label(save_path_frame, text="Where do you want the XML saved to?", font=("Ariel", 12))
-save_path_label.pack(pady=20, anchor="w")
+class SaveDestinationPanel(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.create_widgets()
 
-save_path_entry_frame = tk.Frame(save_path_frame)
-save_path_entry_frame.pack(fill="x", expand=True)
+    def create_widgets(self):
 
-save_path_entry = tk.Entry(save_path_entry_frame)
-save_path_entry.pack(side="left", fill="x", expand=True)
+        self.save_path_label = tk.Label(self, text="Where do you want the XML saved to?", font=("Ariel", 12))
+        self.save_path_label.pack(pady=20, anchor="w")
 
-browse_button = tk.Button(save_path_entry_frame, text="Browse", command=lambda:_browse_folder())
-browse_button.pack(padx=20, side="right")
+        self.save_path_entry_frame = tk.Frame(self)
+        self.save_path_entry_frame.pack(fill="x", expand=True)
 
-def _browse_folder():
-    folder_path = filedialog.askdirectory(title="Select a folder")
+        self.save_path_entry = tk.Entry(self.save_path_entry_frame)
+        self.save_path_entry.pack(side="left", fill="x", expand=True)
 
-    if folder_path:
-        save_path_entry.delete(0, tk.END)
-        save_path_entry.insert(0, folder_path)
+        self.browse_button = tk.Button(self.save_path_entry_frame, text="Browse", command=lambda:self._browse_folder())
+        self.browse_button.pack(side="right")
 
-navigation_frame = tk.Frame(root)
-navigation_frame.pack(padx=20, pady=20)
+    def _browse_folder(self):
+        folder_path = filedialog.askdirectory(title="Select a folder")
+    
+        if folder_path:
+            self.save_path_entry.delete(0, tk.END)
+            self.save_path_entry.insert(0, folder_path)
 
-back_button = tk.Button(navigation_frame, text="Back", command=lambda:_navigate_back())
-back_button.pack(padx=20, side="left")
+class NavigationBar(tk.Frame):
 
-next_button = tk.Button(navigation_frame, text="Next", command=lambda:_navigate_next())
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.create_widgets()
+    
+    def create_widgets(self):
+        navigation_frame = tk.Frame(root)
+        navigation_frame.pack(padx=20, pady=20, anchor="s")
 
-def _navigate_back():
-    pass
+        back_button = tk.Button(navigation_frame, text="Back", command=lambda:self._navigate_back())
+        back_button.pack(padx=20, side="left")
 
-def _navigate_next():
-    pass
+        next_button = tk.Button(navigation_frame, text="Next", command=lambda:self._navigate_next())
+        next_button.pack(padx=20, side="left")
+
+    def _navigate_back(self, event=None):
+        pass
+
+    def _navigate_next(self, event=None):
+        pass
+
+sheet_panel = SheetPanel(root)
+
+save_destination_panel = SaveDestinationPanel(root)
+save_destination_panel.pack(padx=20, pady=20, fill="x", expand=True)
+
+navigation_bar = NavigationBar(root)
 
 root.mainloop()
