7c5b213708316af00bb6feee8a7b8b7894441e99
Rogan Johnston <100793371+Rowgo@users.noreply.github.com>
2026-02-14 00:55:48 -0800

    started fleshing out some widgets for navigation

--- src/gui.py
+++ src/gui.py
@@ -3,9 +3,6 @@ from tkinter import ttk
 from tkinter import filedialog, messagebox
 import json
 
-def browse_folder():
-    pass
-
 root = tk.Tk()
 root.title("Cockatrice Card Set Generator")
 root.geometry("400x300")
@@ -31,7 +28,28 @@ save_path_entry_frame.pack(fill="x", expand=True)
 save_path_entry = tk.Entry(save_path_entry_frame)
 save_path_entry.pack(side="left", fill="x", expand=True)
 
-browse_button = tk.Button(save_path_entry_frame, text="Browse", command=lambda:browse_folder())
+browse_button = tk.Button(save_path_entry_frame, text="Browse", command=lambda:_browse_folder())
 browse_button.pack(padx=20, side="right")
+
+def _browse_folder():
+    folder_path = filedialog.askdirectory(title="Select a folder")
+
+    if folder_path:
+        save_path_entry.delete(0, tk.END)
+        save_path_entry.insert(0, folder_path)
+
+navigation_frame = tk.Frame(root)
+navigation_frame.pack(padx=20, pady=20)
+
+back_button = tk.Button(navigation_frame, text="Back", command=lambda:_navigate_back())
+back_button.pack(padx=20, side="left")
+
+next_button = tk.Button(navigation_frame, text="Next", command=lambda:_navigate_next())
+
+def _navigate_back():
+    pass
+
+def _navigate_next():
+    pass
 
 root.mainloop()
